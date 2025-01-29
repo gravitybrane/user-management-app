a6a475711f589c31c6d0dd729b19a5860131ded9
zboka <zachary.boka@disneystreaming.com>
2025-01-28 20:13:24 -0500

    basic CRUD endpoints at /users

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ Basic CRUD webapp for user management
 
 $ python app.py
 
-http://127.0.0.1:5000
+# Homepage
+
+http://127.0.0.1:5000/
+
+# CRUD endpoints at /users
+Attributes:
+- name
+- email
--- app.py
+++ app.py
@@ -1,6 +1,54 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)  # This initializes the Flask app
+
+# In-memory datastore to managers
+users = {}
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    user_id = len(users) + 1
+    users[user_id] = {
+        "id": user_id,
+        "name": data.get("name"),
+        "email": data.get("email"),
+    }
+    return jsonify(users[user_id]), 201
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    #todo should I return a list(users) ?
+    return jsonify(list(users.values())), 200
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = users.get(user_id)
+    if user:
+        return jsonify(user), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.json
+    user = users.get(user_id)
+    if user:
+        user.update({
+            "name": data.get("name", user["name"]),
+            "email": data.get("email", user["email"]),
+        })
+        return jsonify(user), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    if user_id in users:
+        del users[user_id]
+        return jsonify({"message": "User deleted"}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 @app.route('/')  # This defines a route for the home page ('/')
 def home():
